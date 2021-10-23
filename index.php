3215817ab0b34c72b10ea20421f225be88b6dd64
IonelaRusu <rusuionela06@gmail.com>
2021-10-23 23:28:55 +0300

    add monolog library

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
 
 use App\Game;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 
-require_once realpath("vendor/autoload.php");
+require __DIR__ . "/vendor/autoload.php";
+require __DIR__ . "/src/config.php";
+//require_once realpath("vendor/autoload.php");
+//require_once realpath("src/config.php");
 
-$game = new Game();
+$logger = new Logger("default");
+$logger->pushHandler(new StreamHandler(__DIR__ . "/logs/app.log", Logger::DEBUG));
+
+$game = new Game($logger);
 $game->start();
